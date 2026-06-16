33d04f78029e25495e9dc7d5000c550c7892ae27
thioschris <chrisano2000@gmail.com>
2026-06-16 13:45:42 +0300

    Update site

--- js/footer-map.js
+++ js/footer-map.js
@@ -10,10 +10,11 @@ document.addEventListener('DOMContentLoaded', function() {
     zoom: 15,
     scrollWheelZoom: true,
     zoomControl: true,
+    attributionControl: false,
   });
 
   L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    attribution: '',
     subdomains: 'abcd',
     maxZoom: 19,
   }).addTo(map);
